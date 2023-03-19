e6677e544ae4301cea0da5631c20324125319062
Andrew <me@dragovorn.com>
2023-03-19 18:35:12 -0400

    ignore scratchpad test for now, prepare for publishing 0.1.0

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 import tv.isshoni.winry.api.Winry;
 
@@ -6,6 +7,8 @@ import java.util.concurrent.ExecutionException;
 public class TestRunner {
 
     @Test
+    @Ignore
+
     public void testServer() throws ExecutionException, InterruptedException {
         Winry.bootstrap(TestServer.class);
     }
